45c9343cc57831ea80d03c2d26172bfd2e44c7dd
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2022-08-19 21:35:53 -0500

    carrito cursos y enviar email

---
+++ cursos/javascript/jscarrito/js_07.js
@@ -0,0 +1,125 @@
+//variables
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+let articulosCarrito = [];
+
+
+//eventlisteners
+eventListeners();
+
+function eventListeners(){
+
+    listaCursos.addEventListener('click', agregarCurso);
+    carrito.addEventListener('click', borrarCurso);
+    vaciarCarritoBtn.addEventListener('click', () => {
+
+        articulosCarrito = [];
+        console.log(articulosCarrito);
+        limpiarHTML();
+
+    })
+
+}
+
+
+
+//funciones
+function agregarCurso(e){
+    e.preventDefault();
+
+    if(e.target.classList.contains('agregar-carrito')){
+
+        cursoSeleccionado = e.target.parentElement.parentElement;
+        
+        leerDatosCurso(cursoSeleccionado);
+
+    }
+}
+
+
+function borrarCurso(e){
+
+    if(e.target.classList.contains('borrar-curso')){
+
+        const cursoId = e.target.getAttribute('data-id');
+        
+        articulosCarrito = articulosCarrito.filter(curso => curso.id !== cursoId);
+
+        console.log(articulosCarrito);
+        carritoHTML();
+
+    }
+
+}
+
+
+function leerDatosCurso(curso){
+
+
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+    //actualizamos la cantidad
+    const existe = articulosCarrito.some(curso => curso.id === infoCurso.id);
+
+    if(existe){
+        const cursos = articulosCarrito.map(curso => {
+            if(curso.id === infoCurso.id){
+                curso.cantidad++;
+                return curso;
+            }else{
+                return curso;
+            }
+        });
+
+        articulosCarrito = [...cursos];
+
+    }else{
+
+        articulosCarrito = [...articulosCarrito, infoCurso];
+
+    }
+
+
+    console.log(articulosCarrito);
+
+    carritoHTML();
+}
+
+
+function carritoHTML(){
+
+    limpiarHTML();
+
+    articulosCarrito.forEach(curso => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td><img src="${curso.imagen}" width="50px"></td>
+            <td>${curso.titulo}</td>
+            <td>${curso.precio}</td>
+            <td>${curso.cantidad}</td>
+            <td><a href="#" class="borrar-curso" data-id="${curso.id}">X</a></td>
+        
+        `;
+
+
+        contenedorCarrito.appendChild(row);
+       
+    })
+
+
+}
+
+function limpiarHTML(){
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+
+}
---
+++ cursos/javascript/jssimuladoremail/js_07.js
@@ -0,0 +1,142 @@
+//variables
+const formulario = document.querySelector('#enviar-mail');
+const enviarBtn = document.querySelector('#enviar');
+const resetBtn = document.querySelector('#resetBtn');
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+
+const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const mensajeError = document.createElement('p');
+
+//eventlisteners
+eventListeners();
+
+function eventListeners(){
+
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
+
+    formulario.addEventListener('submit', enviarFormulario);
+    resetBtn.addEventListener('click', resetearFormulario);
+
+}
+
+
+//funciones
+
+function iniciarApp(){
+
+    enviarBtn.disabled = true;
+    enviarBtn.classList.add('disabled');
+
+}
+
+function validarFormulario(e){
+
+    if(e.target.value.length > 0 ){
+
+        const error = document.querySelector('p.error');
+        if(error){
+            error.remove();
+        }
+        
+        e.target.classList.remove('failed');
+        e.target.classList.add('success');
+    }else{
+        e.target.classList.add('failed');
+        e.target.classList.remove('success');
+        mostrarMensaje('Todos los campos son obligatorios')
+
+        iniciarApp();
+    }
+
+
+    if(e.target.type === 'email'){
+        if(er.test(e.target.value)){
+
+            const error = document.querySelector('p.error');
+            if(error){
+                error.remove();
+            }
+            
+            e.target.classList.add('success');
+            e.target.classList.remove('failed');
+
+        }else{
+            e.target.classList.add('failed');
+            e.target.classList.remove('success');
+            mostrarMensaje('Email no Valido');
+            
+            iniciarApp();
+        }
+    }
+
+
+    if(er.test(email.value) && asunto.value !== '' && mensaje.value !== ''){
+        
+        enviarBtn.disabled = false;
+        enviarBtn.classList.remove('disabled');
+
+    }
+
+
+
+}
+
+
+function mostrarMensaje(mensaje){
+    
+    mensajeError.textContent = mensaje;
+    mensajeError.classList.add('mensaje__error', 'error');
+    
+    const errores = document.querySelectorAll('.error');
+
+    if(errores.length === 0){
+        formulario.appendChild(mensajeError);
+    }
+    
+}
+
+
+function enviarFormulario(e){
+    e.preventDefault();
+    
+    
+    const spinner = document.querySelector('.spinner');
+    spinner.style.display = 'flex';
+    spinner.style.justifyContent = 'center';
+    
+    setTimeout(() => {
+        spinner.style.display = 'none';
+
+        const mensajeEnviado = document.createElement('p');
+        mensajeEnviado.textContent = 'Mensaje Enviado Correctamente';
+        mensajeEnviado.classList.add('mensaje__enviado');
+
+        formulario.insertBefore(mensajeEnviado, spinner);
+
+        setTimeout(() => {
+            mensajeEnviado.remove();
+            resetearFormulario();
+
+        }, 1500);
+
+    }, 2500);
+    
+    
+
+}
+
+function resetearFormulario(){
+
+    formulario.reset();
+    iniciarApp();
+    email.classList.remove('failed', 'success');
+    asunto.classList.remove('failed', 'success');
+    mensaje.classList.remove('failed', 'success');
+    mensajeError.remove();  
+
+}
