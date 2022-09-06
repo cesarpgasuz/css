6e3bc02d1556c93d9f697b892f27db5f5cb3364a
cesar <c354r.pgasuz@hotmail.com>
2022-09-06 14:18:52 -0500

    ejercicio localstorage 07

---
+++ cursos/javascript/jslocalstorage/js_07.js
@@ -0,0 +1,128 @@
+//variables
+const formulario = document.querySelector('#formulario');
+const listaTweets = document.querySelector('#lista-tweets');
+let tweets = [];
+
+//eventListeners
+eventListeners();
+
+function eventListeners(){
+
+	formulario.addEventListener('submit', agregarTweet);
+
+	document.addEventListener('DOMContentLoaded', () => {
+
+		tweets = JSON.parse(localStorage.getItem('tweets')) || [];
+		
+		tweetHTML();
+
+	})
+
+
+}
+
+//funciones
+function agregarTweet(e){
+	e.preventDefault();
+
+	const tweet = document.querySelector('#tweet').value;
+
+	if(tweet === ''){
+		mostrarError('No has escrito ningun tweet');
+		return;
+	}
+
+
+	const tweetObj = {
+		texto: tweet,
+		id: Date.now()
+	}
+
+	tweets = [...tweets, tweetObj];
+
+
+	tweetHTML();
+
+
+	formulario.reset();
+
+	console.log(tweets);
+
+
+
+}
+
+
+function mostrarError(mensaje){
+
+	const divMensaje = document.createElement('p');
+	divMensaje.textContent = mensaje;
+	divMensaje.classList.add('mensaje__error', 'error');
+
+	const errores = document.querySelectorAll('.error');
+
+	if(errores.length === 0){
+		const contenido = document.querySelector('#contenido');
+	    contenido.appendChild(divMensaje);
+	}
+	
+	setTimeout(() =>{
+		divMensaje.remove();
+	},2500)
+
+
+}
+
+
+function tweetHTML(){
+
+	limpiarHTML();
+
+	if(tweets.length > 0){
+
+		tweets.forEach(tweet => {
+		const tweetItem = document.createElement('p');
+		tweetItem.textContent = tweet.texto;
+		tweetItem.classList.add('tweet');
+
+		const btnEliminar = document.createElement('button');
+		btnEliminar.classList.add('btn__eliminar');
+		btnEliminar.textContent = 'X';
+		btnEliminar.onclick = () => {
+			eliminarTweet(tweet.id);
+		}
+
+		tweetItem.appendChild(btnEliminar);
+
+
+		listaTweets.appendChild(tweetItem);
+	})
+
+
+	}
+
+	sincronizarStorage();
+
+
+}
+
+function sincronizarStorage(){
+
+	localStorage.setItem('tweets', JSON.stringify(tweets));
+
+}
+
+
+function limpiarHTML(){
+	while(listaTweets.firstChild){
+		listaTweets.removeChild(listaTweets.firstChild);
+	}
+}
+
+function eliminarTweet(id){
+	
+	tweets = tweets.filter(tweet => tweet.id !== id);
+	console.log(tweets);
+	tweetHTML();
+
+}
