0b9b315bfe33306243c847a969e91165afbbe6eb
cesar <c354r.pgasuz@hotmail.com>
2022-09-12 21:23:48 -0500

    ejercicios js 12 septiembre
    modales, observer, carrito, simulador email, buscador, localstorage

---
+++ cursos/javascript/jslocalstorage/js_08.js
@@ -0,0 +1,120 @@
+//variables
+const listaTweets = document.querySelector('#lista-tweets');
+const formulario = document.querySelector('#formulario');
+let tweets = [];
+
+
+//eventListeners
+eventListeners();
+
+function eventListeners(){
+
+	formulario.addEventListener('submit', agregarTweet);
+	document.addEventListener('DOMContentLoaded', () => {
+
+		tweets = JSON.parse(localStorage.getItem('tweets')) || [];
+		tweetHTML();
+
+	})
+
+}
+
+
+//funciones
+
+function agregarTweet(e){
+	e.preventDefault();
+
+	const tweet = document.querySelector('#tweet').value;
+
+	if(tweet === ''){
+		mostrarError('no has escrito ningun tweet');
+		return;
+	}
+
+	const tweetObj = {
+		texto: tweet,
+		id: Date.now()
+	}
+
+	tweets = [...tweets, tweetObj];
+
+
+	console.log(tweets);
+
+	tweetHTML();
+
+	formulario.reset();
+
+
+
+
+}
+
+function mostrarError(mensaje){
+	const mensajeError = document.createElement('p');
+	mensajeError.textContent = mensaje;
+	mensajeError.classList.add('mensaje__error', 'error');
+
+	const errores = document.querySelectorAll('.error');
+	if(errores.length === 0){
+		const contenido = document.querySelector('#contenido');
+		contenido.appendChild(mensajeError);
+	}
+	
+	setTimeout(() => {
+
+		mensajeError.remove();
+
+	}, 3000)
+
+}
+
+
+function tweetHTML(){
+
+	limpiarHTML();
+	
+	if(tweets.length > 0){
+
+		tweets.forEach(tweet => {
+			const tweetItem = document.createElement('p');
+			tweetItem.classList.add('tweet');
+			tweetItem.textContent = tweet.texto;
+
+			const btnEliminar = document.createElement('button');
+			btnEliminar.classList.add('btn__eliminar');
+			btnEliminar.textContent = 'X';
+			btnEliminar.onclick = () => {
+				eliminarTweet(tweet.id);
+			}
+
+			tweetItem.appendChild(btnEliminar);
+
+			listaTweets.appendChild(tweetItem);
+
+		})
+		
+		
+	}
+	sincronizarStorage();
+}
+
+
+function sincronizarStorage(){
+	localStorage.setItem('tweets', JSON.stringify(tweets));
+}
+
+
+function limpiarHTML(){
+	while(listaTweets.firstChild){
+		listaTweets.removeChild(listaTweets.firstChild);
+	}
+}
+
+function eliminarTweet(id){
+
+	tweets = tweets.filter(tweet => tweet.id !== id);
+	tweetHTML();
+
+}
