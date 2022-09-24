4531d62fb3d1fb35c37c55ff88200e892fa7e6c8
cesar <c354r.pgasuz@hotmail.com>
2022-09-24 16:55:07 -0500

    24 de septiembre
    proyecto gasto semanal y administrador de citas

---
+++ cursos/javascript/gasto-semanal/js/app_11.js
@@ -0,0 +1,214 @@
+//variables
+const formulario = document.querySelector('#agregar-gasto');
+const contenedorGastos = document.querySelector('#gastos ul');
+
+
+//eventListeners
+eventListeners();
+
+function eventListeners(){
+
+	document.addEventListener('DOMContentLoaded', pedirPresupuesto);
+	formulario.addEventListener('submit', agregarGasto);
+
+}
+
+//classes
+class Presupuesto{
+	constructor(presupuesto){
+		this.presupuesto = Number(presupuesto);
+		this.restante = Number(presupuesto);
+		this.gastos = [];
+	}
+	nuevoGasto(gasto){
+		this.gastos = [...this.gastos, gasto];
+		console.log(this.gastos);
+		this.actualizarRestante();
+	}
+	actualizarRestante(){
+		const gastado = this.gastos.reduce((total, gasto) => total + gasto.cantidad, 0);
+		this.restante = this.presupuesto - gastado;
+	}
+	eliminarGastos(id){
+		this.gastos = this.gastos.filter(gasto => gasto.id !== id);
+		console.log(this.gastos);
+		this.actualizarRestante();
+	}
+}
+
+
+
+
+
+
+
+
+
+class UI {
+	insertarPresupuesto(cantidad){
+		document.querySelector('#total').textContent = cantidad.presupuesto;
+		document.querySelector('#restante').textContent = cantidad.restante;
+	}
+	mostrarAlerta(mensaje, tipo){
+
+		const divMensaje = document.createElement('div');
+
+		if(tipo === 'error'){
+			divMensaje.classList.add('mensaje__error');
+		}else{
+			divMensaje.classList.add('mensaje__success');
+		}
+
+		divMensaje.textContent = mensaje;
+
+		document.querySelector('.primario').appendChild(divMensaje);
+
+
+		setTimeout(() => {
+			divMensaje.remove();
+		},2500)
+
+	}
+
+	mostrarGastos(gastos){
+
+		this.limpiarHTML();
+
+		gastos.forEach(gasto => {
+			const {nombre, cantidad, id} = gasto;
+
+			const gastoItem = document.createElement('li');
+			gastoItem.dataset.id = id;
+			gastoItem.innerHTML = `${nombre} -- ${cantidad}`;
+
+			const btnEliminar = document.createElement('button');
+			btnEliminar.textContent = 'Eliminar';
+			btnEliminar.classList.add('btn__eliminar');
+			btnEliminar.onclick = () => eliminarGasto(id);
+
+			gastoItem.appendChild(btnEliminar);
+
+			contenedorGastos.appendChild(gastoItem);
+
+
+		})
+
+
+	}
+
+	limpiarHTML(){
+		while(contenedorGastos.firstChild){
+			contenedorGastos.removeChild(contenedorGastos.firstChild);
+		}
+	}
+
+	actualizarRestante(restante){
+		document.querySelector('#restante').textContent = restante;
+	}
+
+	actualizarPresupuesto(presupuestoObj){
+
+		const divRestante = document.querySelector('.restante');
+
+		const {presupuesto, restante} = presupuestoObj;
+
+		if((presupuesto / 4) > restante){
+			divRestante.classList.add('danger');
+			divRestante.classList.remove('success', 'warning');
+		}else if((presupuesto / 2) > restante){
+			divRestante.classList.add('warning');
+			divRestante.classList.remove('danger', 'success');
+		}else{
+			divRestante.classList.add('success');
+			divRestante.classList.remove('danger', 'warning');
+		}
+
+
+		if(restante <= 0){
+			this.mostrarAlerta('Se ha terminado el presupuesto', 'error');
+			formulario.querySelector('button[type="submit"]').disabled = true;
+		}else{
+			formulario.querySelector('button[type="submit"]').disabled = false;
+		}
+
+
+	}
+
+
+
+}
+
+//instanciamos
+const ui = new UI();
+let presupuesto;
+
+
+//funciones
+function pedirPresupuesto(){
+
+	const presupuestoUsuario = prompt('Cual es tu presupuesto');
+
+	if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0){
+		window.location.reload();
+	}
+
+	presupuesto = new Presupuesto(presupuestoUsuario);
+
+	ui.insertarPresupuesto(presupuesto);
+
+
+}
+
+
+
+function agregarGasto(e){
+	e.preventDefault();
+
+	const nombre = document.querySelector('#gasto').value;
+	const cantidad = Number(document.querySelector('#cantidad').value);
+
+	//validamos
+	if(nombre === '' || cantidad === ''){
+		ui.mostrarAlerta('Todos los campos son obligatorios', 'error');
+		return;
+	}else if(cantidad <= 0 || isNaN(cantidad)){
+		ui.mostrarAlerta('Cantidad no Valida', 'error');
+		return;
+	}
+
+	//generamos objecto con el gasto
+	const gasto ={
+		nombre: nombre,
+		cantidad: cantidad,
+		id: Date.now()
+	}
+	
+	presupuesto.nuevoGasto(gasto);
+
+	ui.mostrarAlerta('Gasto Agregado')
+
+	ui.mostrarGastos(presupuesto.gastos);
+
+	ui.actualizarRestante(presupuesto.restante);
+
+	ui.actualizarPresupuesto(presupuesto);
+
+	formulario.reset();
+
+}
+
+
+function eliminarGasto(id){
+
+	presupuesto.eliminarGastos(id);
+
+	ui.mostrarAlerta('Gasto Eliminado');
+
+	ui.mostrarGastos(presupuesto.gastos);
+
+	ui.actualizarRestante(presupuesto.restante);
+
+	ui.actualizarPresupuesto(presupuesto);
+
+
+}
