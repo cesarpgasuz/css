e59ff2e81729a77186236436c9372921810443ab
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2022-09-28 20:43:38 -0500

    28 septiembre
    indexedDB

--- cursos/javascript/CRMIndexedDB/js/app.js
+++ cursos/javascript/CRMIndexedDB/js/app.js
@@ -1,28 +1,35 @@
-(function() {
-
+(function(){
+    //variables
     let DB;
 
+    //eventListeners
     document.addEventListener('DOMContentLoaded', () => {
 
         crearDB();
 
+        if(window.indexedDB.open('crm', 1)){
+            obtenerCliente();
+        }
+
     })
 
-
+    //funciones
+    
     function crearDB(){
         //creamos la base de datos
         const crearDB = window.indexedDB.open('crm', 1);
-        //comprobamos si hay errores
+        //verificacon errores
         crearDB.onerror = function(){
             console.log('hubo un error');
         }
         //
         crearDB.onsuccess = function(){
             console.log('base de datos creada');
-
-            BD = crearDB.result;
-
+            DB = crearDB.result;
+            console.log(DB)
         }
+
+        //configuracion
         crearDB.onupgradeneeded = function(e){
 
             const db = e.target.result;
@@ -45,7 +52,61 @@
 
     }
 
+    function obtenerCliente(){
 
+        const abrirConexion = window.indexedDB.open('crm',1);
+            abrirConexion.onerror = function(){
+                 console.log('hubo un error');
+            };
+
+            abrirConexion.onsuccess = function(){
+
+                DB =abrirConexion.result;
+
+                const objectStore = DB.transaction('crm').objectStore('crm');
+
+                objectStore.openCursor().onsuccess = function(e){
+
+                    const cursor = e.target.result;
+
+                    if(cursor){
+
+                        const {nombre, email, telefono, empresa, id} = cursor.value;
+                        const listadoCliente = document.querySelector('#listado-clientes');
+
+                        listadoCliente.innerHTML += `
+
+                            <tr>
+                                <td>${nombre} - ${email}</td>
+                                <td>${telefono}</td>
+                                <td>${empresa}</td>
+                                <td><a href="editar-cliente.html?id=${id}">Editar</a></td>
+                                <td><a href="#" data-cliente="${id}">Eliminar</a></td>
+
+                            <tr>
+                        
+                        `;
+
+                        cursor.continue();
+
+
+                    }else{
+                        console.log('son todos los datos');
+                    }
+
+
+
+            }
+
+
+
+        }
+
+
+
+
+
+    }
 
 
 
--- cursos/javascript/CRMIndexedDB/js/nuevocliente.js
+++ cursos/javascript/CRMIndexedDB/js/nuevocliente.js
@@ -1,18 +1,35 @@
-(function() {
+(function(){
 
+
+    let DB;
     const formulario = document.querySelector('#formulario');
 
+    //abrimos la conexion
     document.addEventListener('DOMContentLoaded', () => {
 
-        conectarDB();
-
+        abrirConexion();
         formulario.addEventListener('submit', validarFormulario);
 
     })
 
+    function abrirConexion(){
+
+        const abrirConexion = window.indexedDB.open('crm', 1);
+        abrirConexion.onerror = function(){
+            console.log('hubo un error');
+        }
+        abrirConexion.onsuccess = function(){
+            console.log('conexion lista');
+
+            DB = abrirConexion.result;
+
+        }
+
+
+    }
+
 
     function validarFormulario(e){
-
         e.preventDefault();
 
         const nombre = document.querySelector('#nombre').value;
@@ -20,20 +37,74 @@
         const telefono = document.querySelector('#telefono').value;
         const empresa = document.querySelector('#empresa').value;
 
+
         if(nombre === '' || email === '' || telefono === '' || empresa === ''){
             mostrarAlerta('Todos los campos son obligatorios', 'error');
             return;
         }
 
-        console.log('agregando cliente...')
+        const cliente = {
+            nombre: nombre,
+            email: email,
+            telefono: telefono,
+            empresa: empresa,
+            id: Date.now()
+        }
+
+        agregarCLiente(cliente);
 
     }
 
 
+    function agregarCLiente(cliente){
+
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+        objectStore.add(cliente);
+
+        transaction.onerror = function(){
+            mostrarAlerta('hubo un error', 'error');
+        }
+        transaction.oncomplete = function(){
+            mostrarAlerta('cliente agregado')
+
+            setTimeout(() => {
+                window.location.href = 'index.html'
+            }, 800)
+        }
 
 
+    }
+    
 
 
+    function mostrarAlerta(mensaje, tipo){
+
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('alerta');
+
+        const alerta = document.querySelector('.alerta');
+
+        if(!alerta){
+
+            if(tipo === 'error'){
+                divMensaje.classList.add('mensaje__error');
+             }else{
+                divMensaje.classList.add('mensaje__success');
+             }
+
+             divMensaje.textContent = mensaje;
+
+            formulario.appendChild(divMensaje);
+
+            setTimeout(() => {
+                 divMensaje.remove();
+            }, 2500);
+        }
+
+        
+
+    }
 
 
 
